a7cab574e069d672f8205b272c62886922c6c3c4
Dulce Odalys Lujano Valeriano <dulce_luval27@outlook.com>
2024-01-27 21:32:15 -0600

    Se cambio el color de las letras de los botones

--- calculadora2.py
+++ calculadora2.py
@@ -9,9 +9,9 @@ num1=Entry(ventana, font=('times new roman',20,'bold'),width=25,insertwidth=4,bg
 num2=Entry(ventana, font=('times new roman',20,'bold'),width=25,insertwidth=4,bg="#b3b6b7", justify="left").place(x=10,y=60)
 
 #Botones para las operaciones básicas
-Boton_suma=Button(ventana,text="Sumar",width=13,height=3,font=('arial black',10,'bold'),bg="#99cc00").place(x=30,y=110)
-Boton_resta=Button(ventana, text="Restar", width=13,height=3,font=('arial black',10,'bold'),bg="#cc0000").place(x=220,y=110)
-Boton_multiplica=Button(ventana, text="Multiplicar", width=13,height=3,font=('arial black',10,'bold'),bg="#003399").place(x=30,y=210)
-Boton_divide=Button(ventana, text="Dividir", width=13,height=3,font=('arial black',10,'bold'),bg="#663366").place(x=220,y=210)
+Boton_suma=Button(ventana,text="Sumar",width=13,height=3,font=('arial black',10,'bold'),bg="#003300",fg="#ffffff").place(x=30,y=110)
+Boton_resta=Button(ventana, text="Restar", width=13,height=3,font=('arial black',10,'bold'),bg="#660000",fg="#ffffff").place(x=220,y=110)
+Boton_multiplica=Button(ventana, text="Multiplicar", width=13,height=3,font=('arial black',10,'bold'),bg="#000033",fg="#ffffff").place(x=30,y=210)
+Boton_divide=Button(ventana, text="Dividir", width=13,height=3,font=('arial black',10,'bold'),bg="#330000",fg="#ffffff").place(x=220,y=210)
 
 num2=Entry(ventana, font=('times new roman',20,'bold'),width=25,insertwidth=4,bg="#b3b6b7", justify="left").place(x=10,y=300)
