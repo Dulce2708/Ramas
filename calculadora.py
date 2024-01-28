efbfd4256152fc1b33635d5e8771533f8eb7b9dc
Dulce Odalys Lujano Valeriano <dulce_luval27@outlook.com>
2024-01-27 21:21:16 -0600

    Se agrego diseño en la interfaz de la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,13 @@
 from tkinter import *
 ventana = Tk()
 ventana.title("Calculadora sencilla")
-ventana.geometry("400x400")
+ventana.geometry("400x350")
 
-num1=Entry(ventana, font=('times new roman',20,'bold'),width=25,bd=20,insertwidth=4,bg="#b3b6b7", justify="left").place(x=15,y=65)
-num2=Entry(ventana, font=('times new roman',20,'bold'),width=25,bd=20,insertwidth=4,bg="#b3b6b7", justify="left").place(x=15,y=65)
+#Cajas de texto para ingresar los numeros que se van a operar
+num1=Entry(ventana, font=('times new roman',20,'bold'),width=25,insertwidth=4,bg="#b3b6b7", justify="left").place(x=10,y=50)
+num2=Entry(ventana, font=('times new roman',20,'bold'),width=25,insertwidth=4,bg="#b3b6b7", justify="left").place(x=10,y=100)
+
+Boton_suma=Button(ventana,text="Sumar",width=11,height=3,font=('arial black',10,'bold'),bg="#663333").place(x=60,y=180)
+#Boton_resta=Button(ventana, text="Restar", width=11,height=3).place(x=60,y=180)
+#Boton_multiplica=Button(ventana, text="Multiplicar", width=11,height=3).place(x=-20,y=180)
+#Boton_divide=Button(ventana, text="Dividir", width=11,height=3).place(x=-60,y=180)
--- calculadora2.py
+++ calculadora2.py
@@ -1,9 +1,17 @@
 from tkinter import *
 ventana = Tk()
+ventana.configure(background="#330033")
 ventana.title("Calculadora sencilla")
-ventana.geometry("400x400")
+ventana.geometry("370x350")
 
 #Cajas de texto para ingresar los numeros que se van a operar
-num1=Entry(ventana, font=('times new roman',20,'bold'),width=25,bd=10,insertwidth=4,bg="#b3b6b7", justify="left").place(x=10,y=50)
-num2=Entry(ventana, font=('times new roman',20,'bold'),width=25,bd=10,insertwidth=4,bg="#b3b6b7", justify="left").place(x=10,y=100)
+num1=Entry(ventana, font=('times new roman',20,'bold'),width=25,insertwidth=4,bg="#b3b6b7", justify="left").place(x=10,y=15)
+num2=Entry(ventana, font=('times new roman',20,'bold'),width=25,insertwidth=4,bg="#b3b6b7", justify="left").place(x=10,y=60)
 
+#Botones para las operaciones básicas
+Boton_suma=Button(ventana,text="Sumar",width=13,height=3,font=('arial black',10,'bold'),bg="#99cc00").place(x=30,y=110)
+Boton_resta=Button(ventana, text="Restar", width=13,height=3,font=('arial black',10,'bold'),bg="#cc0000").place(x=220,y=110)
+Boton_multiplica=Button(ventana, text="Multiplicar", width=13,height=3,font=('arial black',10,'bold'),bg="#003399").place(x=30,y=210)
+Boton_divide=Button(ventana, text="Dividir", width=13,height=3,font=('arial black',10,'bold'),bg="#663366").place(x=220,y=210)
+
+num2=Entry(ventana, font=('times new roman',20,'bold'),width=25,insertwidth=4,bg="#b3b6b7", justify="left").place(x=10,y=300)
